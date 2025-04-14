6bbffd78320c52f7df28fb94d299303bdaff9494
EmmanuelBaranyk <emmanuelbaranyk4@gmail.com>
2025-04-14 15:14:06 -0400

    working funcion

--- src/copy_content.py
+++ src/copy_content.py
@@ -4,41 +4,22 @@ import shutil
 def copy_content_to_destination(source_dir, destination_dir):
     if not os.path.exists(source_dir):
         raise Exception(f"not a valid src directory: {source_dir}")
-    if os.path.isfile(source_dir):
+    
+    if os.path.isfile(source_dir):  # base case
         return None
     
-    dest_dir = os.path.join(destination_dir, "", "")
     if os.path.exists(destination_dir):          
-        shutil.rmtree(destination_dir)
-        print(f"removed directory: {dest_dir}")
-    
-    
-    #current_dir = os.listdir(path=".")
-    print(f"current directory: {source_dir}")
+        shutil.rmtree(destination_dir)  
 
-    
-    for item in os.listdir(source_dir):
-        print(f"item: {item}")
-        c_path = os.path.join(source_dir, "", item)
-        print(f"path: {c_path}")
-        d_path = os.path.join(dest_dir, "", item)
-        print(f"destination path: {d_path}")    
-
-        try:
-            os.mkdir(dest_dir)  
-            print(f"Directory {dest_dir} created successfully!")
-        except FileExistsError:
-            print(f"Directory {dest_dir} already exists.")
-        except Exception as e:
-            print(f"An error occurred: {e}")
+    os.mkdir(destination_dir)  
+  
+    for item in os.listdir(source_dir):  # iteratethrough items in source dir
+        c_path = os.path.join(source_dir, item)
+        d_path = os.path.join(destination_dir, item)   
         
-        if os.path.isfile(c_path):      
-            print(f"copying item: {item} to d_path: {dest_dir}")
+        if os.path.isfile(c_path):      # if file, copy directly
             shutil.copy(c_path, d_path)
-            print(f"copied item")
         else:
-            print(f"trying recusion...")
-            copy_content_to_destination(c_path, d_path)
-            print(f"recursion successful!")
+            copy_content_to_destination(c_path, d_path)    # if dir, copy contents recursively
     
    
